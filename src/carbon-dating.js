ddcb962808f6c4c130d234273f8eb469df85a43f
syrovezhko nikolai <syrovezhk@list.ru>
2022-04-28 22:53:25 +0300

    passs: Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let output
+  let halfLife
+  if (typeof(sampleActivity) === 'string') {
+    if (parseInt(sampleActivity) > 0) {
+      halfLife = 0.693/HALF_LIFE_PERIOD
+      output = Math.log(MODERN_ACTIVITY/sampleActivity)/halfLife
+      if (output > 0) {
+        return Math.ceil(output)
+      }
+    } 
+  }
+  return false
 }
 
 module.exports = {
