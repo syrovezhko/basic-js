4d3dfdd36ae61a37d3b659e2cb295886b21c20c5
syrovezhko nikolai <syrovezhk@list.ru>
2022-04-28 21:59:08 +0300

    passs: Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+ function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(members) !== true){
+    return 0;
+  }
+  let string = '';
+  let arr = [];
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] === "string") {
+      let a = members[i].trim();
+      arr.push(a[0].toUpperCase());
+    }
+  };
+  arr = arr.sort();
+  console.log(arr)
+  for(let i = 0; i < arr.length; i++) {
+    string += arr[i];
+  }
+  return string;
 }
 
 module.exports = {
