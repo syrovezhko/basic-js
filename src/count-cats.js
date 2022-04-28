f43f24fbcdecfa194c7328883a281fcb30fef3aa
syrovezhko nikolai <syrovezhk@list.ru>
2022-04-28 20:03:51 +0300

    passs: countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let number = 0;
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i].length; j++){
+      if (arr[i][j] === '^^') {
+        number++;
+      }
+    }
+  }
+  return number;
 }
 
 module.exports = {
